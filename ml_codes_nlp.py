583afc79579ab10f40ff9ae2c7bc75d5d88568a2
pouniq <abdollahpora@gmail.com>
2026-09-11 00:21:14 +0330

    added vader sentiment analysis

---
+++ ml_codes_nlp.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+# create a list of sentences
+data = [
+    "When life gives you lemons, make lemonade! 🙂",
+    "She bought 2 lemons for $1 at Maven Market.",
+    "A dozen lemons will make a gallon of lemonade. [AllRecipes]",
+    "Lemon, lemon, lemons, lemon, lemon, lemons",
+    "He's running to the market to get a lemon - there's a great sale today.",
+    "iced tea is my favorite",
+    "I didn't like the taste of that lemonade at all.",
+    "My lemons went bad before I could use them, unfortunately."
+]
+# expand the column width to see the full sentences
+pd.set_option('display.max_colwidth', None)
+# turn it into a dataframe
+data_df = pd.DataFrame(data, columns=["sentence"])
+data_df.head()
+# make a copy of the dataframe
+df = data_df.copy()
+
+df.head()
+
+test = df.sentence[0]
+model = SentimentIntensityAnalyzer()
+model.polarity_scores(test)
+
+model.polarity_scores(test)['compound']
+
+def get_sentiment(text):
+   model = SentimentIntensityAnalyzer()
+   output = model.polarity_scores(text)['compound']
+   return output
+    
+
+df['sentiment_compound'] = df.sentence.apply(get_sentiment)
